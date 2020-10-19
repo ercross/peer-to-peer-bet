9cb5e24ec86a86257b63decf697552716b18b895
Ercross <tobins4u@gmail.com>
2020-10-19 07:04:44 +0100

    Update _PeerToPeerBet.sol

--- _PeerToPeerBet.sol
+++ _PeerToPeerBet.sol
@@ -43,7 +43,7 @@ contract PeerToPeerBet {
         uint _eventStartsIn,
         BetCategory _betCategory, 
         string[] memory _predictedOutcomes,
-        string memory _predictedOutcome,
+        string memory _predictedOutcome
         ) 
         public {
             creator = msg.sender;
@@ -54,41 +54,59 @@ contract PeerToPeerBet {
             eventStartsIn = _eventStartsIn;
             predictedOutcomes = _predictedOutcomes;
             PlayerBetData creatorBetData = PlayerBetData ({
-                stakeAmount: msg.value;
+                stakeAmount: msg.value,
                 predictedOutcome: _predictedOutcome
-            })
+            });
         }
         
     /**
-     * 
+     * @notice joinBet allows other players to join in to this bet contract
     */
     function joinBet(string memory _predictedOutcome) public payable {
       require (msg.value > minimumStakeAmount);  
-      require (players[msg.sender] == "")
-      PlayerBetData playerBetData = playerBetData({
+      require (players[msg.sender] == "");
+      PlayerBetData memory playerBetData = playerBetData({
           predictedOutcome: _predictedOutcome,
           stakeAmount: msg.value
-      })
+      });
       players[msg.sender] = playerBetData;
       numberOfPlayers++;
     }
     
     /**
-     * 
+     * @param _actualOutcome to be set by address overseer
     */
     function settleBet (string memory _actualOutcome) public {
-        require (msg.sender == overseer)
+        require (msg.sender == overseer);
         actualOutcome = _actualOutcome;
         []address winningPlayers = fetchAllWinners(calldata _actualOutcome);
-        payWinners(winningPlayers);
+        payWinners(calldata winningPlayers);
     }
     
     /**
-     * 
+     * @notice cancelBet cancels this bet provided no other player has joined
     */
     function cancelBet() public {
         require (msg.sender == creator);
         require (numberOfPlayers == 1); 
         creator.transfer(players[creator].stakeAmount);
+    }
+    
+    function fetchAllWinners (string memory _actualOutcome) internal returns (address[] winners) {
+        playerBetData memory sender = playerBetData[msg.sender];
+        for(uint i=0; i<players; i++) {
+            if (players.address.predictedOutcome = _actualOutcome) {
+                winners.push[players.address];
+            }
+        }
+        return winners;
+    }
+    
+    function payWinners (string[] memory _winningplayers ) internal payable {
+        require(getProt(msg.sender) > propThres());
+        uint winPerPerson = totalStakesAmount / _winningplayers.length
+        for (uint i=0; i<_winningplayers.length; i++) {
+            _winningplayers[i].transfer(winPerPerson);
+        }
     }
 }
